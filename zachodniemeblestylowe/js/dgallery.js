a98e3757db014677342194511425172c80910a90
Witold Kręblewski <wkreblewski@gmail.com>
2018-02-01 14:30:54 +0100

    main gallery and first side gallery done

--- zachodniemeblestylowe/js/dgallery.js
+++ zachodniemeblestylowe/js/dgallery.js
@@ -9,8 +9,7 @@ document.addEventListener('DOMContentLoaded', function(){
     var body = document.querySelector("body");
     console.log(body);
 
-    var prev = document.querySelector("#prevPicture")
-    console.log(prev);
+
 
 
     //
@@ -19,35 +18,40 @@ document.addEventListener('DOMContentLoaded', function(){
         all[i].addEventListener('click', function(){
             var src = this.querySelector("img").getAttribute("src");
             var	newDiv	=	document.createElement("div");
-            newDiv.innerHTML='<button class="prevPicture">Poprzednie</button>'+'<button class="close">Zamkni</button>'+'<button class="nextPicture">Następnye</button>'+'<img src="./'+src+'">';
+            // newDiv.innerHTML='<button class="prevPicture">Poprzednie</button>'+'<button class="close">Zamkni</button>'+'<button class="nextPicture">Następnye</button>'+'<img src="./'+src+'">';
+            newDiv.innerHTML='<img src="./'+src+'">';
             newDiv.classList.add("fullScreen");
             body.appendChild(newDiv);
 
-            var next = document.querySelector(".nextPicture")
-            console.log(next+"to jest nexttttttttttttttttttttttttttt");
+            // var next = document.querySelector(".nextPicture")
+            // console.log(next+"to jest nexttttttttttttttttttttttttttt");
+            //
+            // var prev = document.querySelector(".prevPicture")
+            // console.log(prev);
 
             var button = document.querySelector(".close");
             console.log(button);
 
-            button.addEventListener('click', function(){
+            newDiv.addEventListener('click', function(){
                 var toDelete = document.querySelector(".fullScreen");
                 console.log(toDelete);
                 toDelete.parentElement.removeChild(toDelete);
             })
 
-            var counter = 0;
-
-            next.addEventListener('click', function(){
-                console.log("click next")
-                counter++
-                // if (counter >= all.length){
-                //     counter = 0
-                // }
-                newDiv.innerHTML='<button class="prevPicture">Poprzednie</button>'+'<button class="close">Zamkni</button>'+'<button class="nextPicture">Następnye</button>'+'' +
-                    '<img src="images/dphotos/'+counter+'.JPG">';
-
-                // all[counter].classList.add("visible");
-            })
+            // var counter = 0;
+            //
+            // next.addEventListener('click', function(){
+            //     console.log("click next")
+            //     src[i]
+            //
+            //     counter++
+            //     // if (counter >= all.length){
+            //     //     counter = 0
+            //     // }
+            //     newDiv.innerHTML='<button class="prevPicture">Poprzednie</button>'+'<button class="close">Zamkni</button>'+'<button class="nextPicture">Następnye</button>'+'' +
+            //         '<img src="'+src+'"/>';
+            //     // all[counter].classList.add("visible");
+            // })
 
 
             // newDiv.addEventListener('click', function(){
